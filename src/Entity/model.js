d2e0bded08167a8ce426d2e0d6d05ad95e56b1e8
rodrigo lucas <rodrigolucassoares@gmail.com>
2021-06-11 16:40:41 -0300

    removed example curve from model lines

--- src/Entity/model.js
+++ src/Entity/model.js
@@ -1,11 +1,11 @@
 export default class Model {
     constructor(){
         this.lines = [
-            
+            /* 
             {
                 pt0: [1.0, 1.0],
                 pt1: [2.0, 2.0]
-            }
+            } */
            
         ];
     }
